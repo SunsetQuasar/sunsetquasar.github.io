4fabfb26792f5b30f00f7618f72789f94fb8ca3f
xestevaogspx@gmail.com <xestevaogspx@gmail.com>
2025-04-09 16:55:25 -0400

    update! should be good

--- CelesteBrainstorm/main.js
+++ CelesteBrainstorm/main.js
@@ -6,10 +6,28 @@ function loadContent() {
         )
     )
 }
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
+
+modsClean = [];
+elements = [];
+chosenElements = [];
+
 function main(json) {
     var mods = json.modInfo;
-    var modsClean = [];
-    elements = [];
     document.getElementById("result").innerHTML = "";
     for (var i = 0; i < mods.length; i++) {
         if (mods[i].categoryName != 'Map') {
@@ -27,12 +45,39 @@ function main(json) {
         }
     }
 
-    document.getElementById("result").innerHTML = "<h2>Found " + elements.length + " entities from " + modsClean.length + " mods</h2>";
+    var startElement = document.getElementById("start")
 
-    for (var i = 0; i < 10; i++) {
-        var entity = elements[Math.floor(Math.random() * elements.length)]
-        document.getElementById("result").innerHTML += "<p>" + entity.name + " | <a href=\"https://gamebanana.com/" + entity.modItemType.toLowerCase() + "s" + "/" + entity.modID + "\">" + entity.mod + " v" + entity.latestModVersion + "</a></p>";
+    startElement.innerHTML = "<h2>Found " + elements.length + " entities from " + modsClean.length + " mods</h2>";
+    startElement.innerHTML += "<label for=\"entityNum\">Number of entities to generate:</label><input type=\"number\" id=\"entityNum\" name=\"entityNum\" min=\"1\" max=\"" + elements.length + "\" value=\"5\"/>";
+    startElement.innerHTML += "<div style=\"padding: 16px\"><button type=\"button\" onclick=\"generateNew()\">Generate</button></div>"
+}
+
+function generateNew() {
+    var max = parseInt(document.getElementById("entityNum").value);
+    shuffle(elements);
+    chosenElements = elements.slice(0, max);
+    displayEntities(chosenElements);
+}
+
+function displayEntities(entities) {
+
+    document.getElementById("result").innerHTML = "";
+
+    for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+
+        var name = entity.name.split(" / ");
+        document.getElementById("result").innerHTML +=
+            "<p>" + name[0] + " | " +
+            "<a href=\"https://gamebanana.com/" + entity.modItemType.toLowerCase() + "s" + "/" + entity.modID + "\">" + entity.mod + " v" + entity.latestModVersion + "</a>" +
+            " | <button type=\"button\" onclick=\"rerollElement(" + i + ")\">Reroll</button></p>";
     }
+}
+
+function rerollElement(id) {
+    shuffle(elements);
+    chosenElements[id] = elements[0];
+    displayEntities(chosenElements);
 }
 
 loadContent();
